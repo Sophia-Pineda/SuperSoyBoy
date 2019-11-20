5e9ffbdb324a197b561b4b871cd2820e02551b6d
Sophia Pineda <sophia.pineda@hotmail.com>
2019-11-20 12:56:45 -0500

    More level enhancements

---
+++ Assets/Scripts/OrderI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderI : MonoBehaviour
+{
+    public Vector3 OrderSixtySix = Vector3.left;
+
+
+    void update()
+    {
+        SwayForward();
+    }
+
+    void SwayForward()
+    {
+        if (this.transform.position.x > -2.0f)
+        {
+            OrderSixtySix = Vector3.left;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+
+        this.transform.Translate(OrderSixtySix * Time.smoothDeltaTime);
+
+    }
+
+}
